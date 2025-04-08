79b9b225d0cbecf75c6e67180a89773a6e67dd1b
Yun-Jiyeol <yir0612@gmail.com>
2025-04-08 17:59:22 +0900

    [Feat] 마우스 좌표 인식 시키기

--- Assets/02.Scripts/Character/Player/Player.cs
+++ Assets/02.Scripts/Character/Player/Player.cs
@@ -6,12 +6,17 @@ public class Player : MonoBehaviour
 {
     public PlayerController playerController;
     public PlayerStat stat;
+    public CheckFieldOnMouse checkFieldOnMouse;
+
+    public GameObject mouseFollower;
+
     public Camera MainCamera; // 임시 카메라(GameManager나 CameraManager에서 받아올 것)
 
     private void Awake()
     {
         playerController = GetComponent<PlayerController>();
         stat = GetComponent<PlayerStat>();
+        checkFieldOnMouse = GetComponent<CheckFieldOnMouse>();
     }
 
     private void Start()
--- Assets/02.Scripts/Character/Player/PlayerController.cs
+++ Assets/02.Scripts/Character/Player/PlayerController.cs
@@ -6,21 +6,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : BaseController
 {
-    private float ActiveRange;
-    Camera camera;
-
-    private void Start()
-    {
-        camera = gameObject.GetComponent<Player>().MainCamera.GetComponent<Camera>();
-
-        settActiveRange();
-    }
-
-    public void settActiveRange()
-    {
-        ActiveRange = gameObject.GetComponent<Player>().stat.ActiveRange;
-    }
-
     void OnMove(InputValue inputValue)
     {
         dir = inputValue.Get<Vector2>();
@@ -30,15 +15,7 @@ public class PlayerController : BaseController
     {
         if (inputValue.isPressed)
         {
-            Vector2 MousePosition = Input.mousePosition;
-            MousePosition = camera.ScreenToWorldPoint(MousePosition);
 
-            Debug.Log(MousePosition);
-
-            if(Vector2.Distance(MousePosition,transform.position) <= ActiveRange)
-            {
-                Debug.Log("사거리 내");
-            }
         }
     }
 }
