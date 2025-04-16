ff9b90b7b54d50860c4a43e7ca5c835c74ae0de9
yjyj713 <yjyj713@naver.com>
2025-04-16 20:33:42 +0900

    [Feat] Quest버튼 동작

---
+++ Assets/02.Scripts/04.UI/QuestUI.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestUI : UIBase
+{
+    public Button QuestBtn;
+    public Button CloseBtn;
+    public GameObject Quest;
+    public void SetUp()
+    {
+        //setup()Áßº¹ ¹æÁö
+        QuestBtn.onClick.RemoveAllListeners();
+        CloseBtn.onClick.RemoveAllListeners();
+
+        QuestBtn.onClick.AddListener(OnQuestBtn);
+        CloseBtn.onClick.AddListener(OnCloseBtn);
+
+        Quest?.SetActive(false);
+        QuestBtn.gameObject.SetActive(true);
+    }
+
+    public void Awake()
+    {
+        SetUp();
+    }
+
+    public override void Show()
+    {
+
+        Quest?.SetActive(true);
+        QuestBtn.gameObject.SetActive(false);
+    }
+
+    public override void Hide()
+    {
+
+        Quest?.SetActive(false);
+        QuestBtn.gameObject.SetActive(true);
+
+    }
+    public void OnQuestBtn()
+    {
+        Debug.Log("Äù½ºÆ® ¹öÆ° Å¬¸¯µÊ");
+        Show();
+    }
+    public void OnCloseBtn()
+    {
+        Debug.Log("Äù½ºÆ® ´Ý±â ¹öÆ° Å¬¸¯µÊ");
+        Hide();
+    }
+}
