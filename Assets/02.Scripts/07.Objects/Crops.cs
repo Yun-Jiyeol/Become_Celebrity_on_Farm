819ca90d212984596462c15c1b0cb2f8c77d1644
000SIN <126735143+000SIN@users.noreply.github.com>
2025-04-18 01:54:47 +0900

    [Feat] Asset 추가 및 sky 제작

--- Assets/02.Scripts/07.Objects/Crops.cs
+++ Assets/02.Scripts/07.Objects/Crops.cs
@@ -1,53 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Crops : SeedGrow
-{
-    public bool isDestroyAfterHarvest;
-    public int AfterHarvest = 0;
+//public class Crops : SeedGrow
+//{
+//    public bool isDestroyAfterHarvest;
+//    public int AfterHarvest = 0;
 
-    protected override void Start()
-    {
-        base.Start();
-    }
+//    protected override void Start()
+//    {
+//        base.Start();
+//    }
 
-    public override void Grow(float grow)
-    {
-        GetDamage(grow);
-        CheckGrow();
-    }
+//    public override void Grow(float grow)
+//    {
+//        GetDamage(grow);
+//        CheckGrow();
+//    }
 
-    public override void CheckGrow()
-    {
-        base.CheckGrow();
-    }
+//    public override void CheckGrow()
+//    {
+//        base.CheckGrow();
+//    }
 
-    protected override void calledInteract()
-    {
-        if (!isEndGrow) return;
+//    protected override void calledInteract()
+//    {
+//        if (!isEndGrow) return;
 
-        if(SpawnItemAmount != 0)
-        {
-            ItemManager.Instance.spawnItem.DropItem(ItemManager.Instance.itemDataReader.itemsDatas[SpawnItemNum], SpawnItemAmount, gameObject.transform.position);
-        }
-        if (isDestroyAfterHarvest)
-        {
-            GameManager.Instance.CanInteractionObjects["SeededGround"].Remove(gameObject);
-            Destroy(gameObject);
-        }
-        else
-        {
-            GetDamage(-AfterHarvest);
-            CheckGrow();
-            transform.tag = "Seeded";
-        }
-    }
+//        if(SpawnItemAmount != 0)
+//        {
+//            ItemManager.Instance.spawnItem.DropItem(ItemManager.Instance.itemDataReader.itemsDatas[SpawnItemNum], SpawnItemAmount, gameObject.transform.position);
+//        }
+//        if (isDestroyAfterHarvest)
+//        {
+//            GameManager.Instance.CanInteractionObjects["SeededGround"].Remove(gameObject);
+//            Destroy(gameObject);
+//        }
+//        else
+//        {
+//            GetDamage(-AfterHarvest);
+//            CheckGrow();
+//            transform.tag = "Seeded";
+//        }
+//    }
 
-    public override void HandInteract()
-    {
-        base.HandInteract();
+//    public override void HandInteract()
+//    {
+//        base.HandInteract();
 
-        calledInteract();
-    }
-}
+//        calledInteract();
+//    }
+//}
