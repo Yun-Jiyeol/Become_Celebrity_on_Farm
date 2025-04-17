dcd9e17e1bebde90a4f3a0671c903607c6739ea7
yjyj713 <yjyj713@naver.com>
2025-04-17 19:27:07 +0900

    [Fix] 골드ui스크립트 수정

--- Assets/02.Scripts/04.UI/GoldUI.cs
+++ Assets/02.Scripts/04.UI/GoldUI.cs
@@ -6,31 +6,25 @@ public class GoldUI : UIBase
 {
     [SerializeField] private TextMeshProUGUI goldText;
 
-    private void Start() //게임 시작시 골드 이벤트 구독, 현재 골드로 ui초기화
+    private void OnEnable()
     {
-        if(GoldManager.Instance != null)
+        if (GoldManager.Instance != null)
         {
             GoldManager.Instance.OnGoldChanged += UpdateUI;
-            UpdateUI(GoldManager.Instance.CurGold); // 시작 시 초기화
+            UpdateUI(GoldManager.Instance.GetGold());
         }
     }
 
-    private void OnDestroy() //오브젝트가 제거될때 이벤트 구독 해제
+    private void OnDisable()
     {
-        if(GoldManager.Instance != null)
+        if (GoldManager.Instance != null)
         {
             GoldManager.Instance.OnGoldChanged -= UpdateUI;
         }
     }
 
-    private void UpdateUI(int gold) //골드 값을 받아 텍스트에 반영
+    private void UpdateUI(int value)
     {
-        goldText.text = gold.ToString("0000000");
-    }
-
-    public override void Show()
-    {
-        base.Show();
-        UpdateUI(GoldManager.Instance.CurGold);
+        goldText.text = value.ToString("N0"); // 천 단위 쉼표
     }
 }
