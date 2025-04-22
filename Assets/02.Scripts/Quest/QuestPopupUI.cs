e2b6829825e2f8b770c92edf3eed83bb48cea452
yjyj713 <yjyj713@naver.com>
2025-04-22 16:44:10 +0900

    [Feat] 퀘스트 스크립트들

---
+++ Assets/02.Scripts/Quest/QuestPopupUI.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestPopupUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private Image questIcon;
+    [SerializeField] private Button acceptButton;
+    [SerializeField] private Button declineButton;
+
+    private QuestData currentQuest;
+
+    private void Awake()
+    {
+        acceptButton.onClick.AddListener(OnAccept);
+        declineButton.onClick.AddListener(OnDecline);
+
+        Hide();
+    }
+
+    public void Show(QuestData quest)
+    {
+        currentQuest = quest;
+
+        titleText.text = quest.questTitle;
+        descriptionText.text = quest.questDescription;
+        //questIcon.sprite = quest.questIcon;
+
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnAccept()
+    {
+        QuestManager.Instance.AcceptQuest();
+        Hide();
+    }
+
+    private void OnDecline()
+    {
+        QuestManager.Instance.DeclineQuest();
+        Hide();
+    }
+}
---
+++ Assets/02.Scripts/Quest/QuestSlotUI.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestSlotUI : MonoBehaviour
+{
+    [SerializeField] private Transform slotParent;             // 슬롯들이 들어갈 부모
+    [SerializeField] private GameObject questSlotPrefab;       // 슬롯 프리팹
+
+    private List<GameObject> activeSlots = new List<GameObject>();
+    private const int maxSlots = 3;
+
+    public bool HasQuest => activeSlots.Count >= maxSlots;
+
+    public void Assign(QuestData quest)
+    {
+        AddQuestSlot(quest);
+    }
+
+    /// <summary>
+    /// 퀘스트 슬롯 초기화 (초기엔 비워둠)
+    /// </summary>
+    public void ClearSlots()
+    {
+        foreach (var slot in activeSlots)
+        {
+            Destroy(slot);
+        }
+        activeSlots.Clear();
+    }
+
+    /// <summary>
+    /// 새로운 퀘스트를 슬롯에 추가함 (최대 3개 제한)
+    /// </summary>
+    public void AddQuestSlot(QuestData quest)
+    {
+        if (activeSlots.Count >= maxSlots)
+        {
+            Debug.Log("[QuestSlotUI] 퀘스트 슬롯이 가득 찼습니다.");
+            return;
+        }
+
+        GameObject newSlot = Instantiate(questSlotPrefab, slotParent);
+        QuestSlotElement element = newSlot.GetComponent<QuestSlotElement>();
+        if (element != null)
+        {
+            element.SetData(quest);
+        }
+
+        activeSlots.Add(newSlot);
+    }
+}
