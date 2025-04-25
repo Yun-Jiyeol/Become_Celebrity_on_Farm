5687c4b7368b6ec4ba6bbda4770c55425f906ca1
S-SoHyun <slj506@naver.com>
2025-04-25 17:40:08 +0900

    [Chore] Summer, Fall, Winter 팔레트 생성

---
+++ Assets/02.Scripts/10. Map/SeasonTileChanger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using static Season;
+
+
+[System.Serializable]
+public class SeasonTile
+{
+    public SeasonType season;
+    public TileBase tile;
+}
+
+[CreateAssetMenu(fileName = "SeasonTile", menuName = "Create SeasonTile")]
+public class SeasonTileList : ScriptableObject
+{
+    public List<SeasonTile> seasonTiles;
+}
+
+
+
+/// <summary>
+/// 계절에 따라 달라질 타일
+/// </summary>
+
+public class SeasonTileChanger : MonoBehaviour
+{
+    Season season;
+    //List<>
+
+    //OnSeasonChanged
+
+    //void Initialized()
+    //{
+    //    // 시즌 구독
+    //    season.UpdateSeason() -= SeasonChanged;
+    //    season.UpdateSeason() += SeasonChanged;
+    //}
+
+    void SeasonChanged()
+    {
+
+    }
+
+
+}
