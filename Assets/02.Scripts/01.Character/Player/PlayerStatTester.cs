3853bb3d658c827d9e2bdfe167c2f9eca292afdc
yjyj713 <yjyj713@naver.com>
2025-04-28 20:57:04 +0900

    [Feat] 플레이어 스탯 테스트용 스크립트 생성

---
+++ Assets/02.Scripts/01.Character/Player/PlayerStatTester.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatTester : MonoBehaviour
+{
+    [SerializeField] private Button hpDecreaseButton;
+    [SerializeField] private Button hpIncreaseButton;
+    [SerializeField] private Button manaDecreaseButton;
+    [SerializeField] private Button manaIncreaseButton;
+
+    private PlayerStats player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerStats>();
+        if (player == null)
+        {
+            Debug.LogError("[PlayerStatTester] PlayerStats 찾을 수 없음!");
+            return;
+        }
+
+        hpDecreaseButton.onClick.AddListener(() => ChangeHp(-10f));
+        hpIncreaseButton.onClick.AddListener(() => ChangeHp(+10f));
+        manaDecreaseButton.onClick.AddListener(() => ChangeMana(-10f));
+        manaIncreaseButton.onClick.AddListener(() => ChangeMana(+10f));
+    }
+
+    private void ChangeHp(float amount)
+    {
+        player.Hp = Mathf.Clamp(player.Hp + amount, 0, player.MaxHp);
+    }
+
+    private void ChangeMana(float amount)
+    {
+        player.Mana = Mathf.Clamp(player.Mana + amount, 0, player.MaxMana);
+    }
+}
