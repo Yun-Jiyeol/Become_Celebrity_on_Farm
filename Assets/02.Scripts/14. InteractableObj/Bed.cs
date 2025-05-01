cd538d797320cc31510501c3216e5961ef0d7655
S-SoHyun <slj506@naver.com>
2025-05-01 15:51:04 +0900

    [Feat] EndOfDayUI 추가

---
+++ Assets/02.Scripts/14. InteractableObj/Bed.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Bed : MonoBehaviour
+{
+    [Header("Fader")]
+    [SerializeField] private LoadingFader fader;
+
+    [Header("UI")]
+    [SerializeField] private Image endOfDaySelectUI;
+    [SerializeField] private Canvas endingUI;
+
+    [Header("Buttons")]
+    [SerializeField] private Button yesButton;
+    [SerializeField] private Button noButton;
+
+
+
+    private void Start()
+    {
+        yesButton.onClick.AddListener(OnClickYesButton);
+        noButton.onClick.AddListener(OnClickNoButton);
+
+        endOfDaySelectUI.gameObject.SetActive(false);
+        endingUI.gameObject.SetActive(false);
+    }
+
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // PlayerController?
+
+        if (GameManager.Instance.player.TryGetComponent(out PlayerInput input))
+        {
+            input.enabled = false;
+        }
+
+        endOfDaySelectUI.gameObject.SetActive(true);
+    }
+
+
+    void OnClickYesButton()
+    {
+        endOfDaySelectUI.gameObject.SetActive(false);
+
+        StartCoroutine(fader.Fade(
+            () =>
+            {
+                endingUI.gameObject.SetActive(true);
+            },
+
+            () =>
+            {
+                // 다음 날 진행?
+            }
+            ));
+    }
+
+    void OnClickNoButton()
+    {
+        endOfDaySelectUI.gameObject.SetActive(false);
+
+        if (GameManager.Instance.player.TryGetComponent(out PlayerInput input))
+        {
+            input.enabled = true;
+        }
+    }
+}
---
+++ Assets/02.Scripts/14. InteractableObj/Computer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Computer : MonoBehaviour, IPointerClickHandler
+{
+    [Header("UI")]
+    [SerializeField] private Image dailyQuestUI;
+
+    [Header("Button")]
+    [SerializeField] private Button yesButton;
+    [SerializeField] private Button closeButton;
+
+
+    private void Start()
+    {
+        yesButton.onClick.AddListener(OnClickYesButton);
+        closeButton.onClick.AddListener(OnClickCloseButton);
+
+        dailyQuestUI.gameObject.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // ?
+        dailyQuestUI.gameObject.SetActive(true);
+        if (GameManager.Instance.player.TryGetComponent(out PlayerInput input))
+        {
+            input.enabled = false;
+        }
+        Debug.Log("Click");
+    }
+
+    void OnClickYesButton()
+    {
+        // 퀘스트 받음
+
+        dailyQuestUI.gameObject.SetActive(false);
+        if (GameManager.Instance.player.TryGetComponent(out PlayerInput input))
+        {
+            input.enabled = true;
+        }
+    }
+
+
+    void OnClickCloseButton()
+    {
+        dailyQuestUI.gameObject.SetActive(false);
+
+        if (GameManager.Instance.player.TryGetComponent(out PlayerInput input))
+        {
+            input.enabled = true;
+        }
+    }
+}
