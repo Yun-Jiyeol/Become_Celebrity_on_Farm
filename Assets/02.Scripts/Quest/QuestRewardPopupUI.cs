ce60389ae3702ac43eed28d10182b17a8f664938
yjyj713 <yjyj713@naver.com>
2025-05-01 11:06:08 +0900

    [Feat] 퀘스트완료 보상팝업

---
+++ Assets/02.Scripts/Quest/QuestRewardPopupUI.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class QuestRewardPopupUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI questTitleTxt;
+    [SerializeField] private TextMeshProUGUI rewardTxt;
+    [SerializeField] private Button confirmButton;
+
+    private QuestData currentQuest;
+
+    private void Awake()
+    {
+        confirmButton.onClick.AddListener(OnConfirm); // 버튼 클릭 시 보상 지급
+        gameObject.SetActive(false);
+    }
+
+    public void Show(QuestData quest)
+    {
+        currentQuest = quest;
+        questTitleTxt.text = quest.questTitle;
+        rewardTxt.text = $"골드 : {quest.rewardGold} \n경험치 : {quest.rewardExp}";
+
+        gameObject.SetActive(true);
+    }
+
+    private void OnConfirm()
+    {
+        if (currentQuest != null)
+        {
+            //보상 지급
+            PlayerStats player = FindObjectOfType<PlayerStats>();
+            if (player != null)
+            {
+                player.AddGold(currentQuest.rewardGold);
+                // 경험치 시스템 추가
+            }
+
+            currentQuest = null;
+        }
+
+        gameObject.SetActive(false);
+    }
+}
