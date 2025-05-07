ad798c02c3ebb7736a3121a4f76eb156848507f1
Yun-Jiyeol <yir0612@gmail.com>
2025-05-07 23:18:24 +0900

    [Feat] CraftTable 세팅

---
+++ Assets/02.Scripts/02.Item/CraftManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftManager : MonoBehaviour
+{
+    public List<GameObject> ListOfCraftingTable;
+}
---
+++ Assets/02.Scripts/02.Item/CraftingScroll.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SettingCraft
+{
+    public GameObject Line;
+    public RectTransform Point;
+}
+
+public class CraftingScroll : MonoBehaviour
+{
+    public RectTransform ScrollMain;
+    public List<SettingCraft> settingCrafts;
+
+    public void AddCraftTable()
+    {
+
+    }
+
+
+    GameObject FindShortestLine()
+    {
+        float min = settingCrafts[0].Point.position.y;
+        GameObject minPoint = settingCrafts[0].Line;
+
+        for(int i =1; i<settingCrafts.Count; i++)
+        {
+            if (settingCrafts[i].Point.position.y < min)
+            {
+                min = settingCrafts[i].Point.position.y;
+                minPoint = settingCrafts[i].Line;
+            }
+        }
+        return minPoint;
+    }
+
+    RectTransform FindLongestLine()
+    {
+        float max = settingCrafts[0].Point.position.y;
+        RectTransform minPoint = settingCrafts[0].Point;
+
+        for (int i = 1; i < settingCrafts.Count; i++)
+        {
+            if (settingCrafts[i].Point.position.y >= max)
+            {
+                max = settingCrafts[i].Point.position.y;
+                minPoint = settingCrafts[i].Point;
+            }
+        }
+        return minPoint;
+    }
+}
